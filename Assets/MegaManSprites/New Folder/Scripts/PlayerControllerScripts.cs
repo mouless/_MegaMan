59808a4aee55eb58e3acc63ba6128f0f35c69fe4
wibo <william.boquist@gmail.com>
2017-09-07 16:34:26 +0200

    Tror jag har fixat något med hopp saken, men den spelar inte ut hela animationen när man hoppar och skjuter :(

--- Assets/MegaManSprites/New Folder/Scripts/PlayerControllerScripts.cs
+++ Assets/MegaManSprites/New Folder/Scripts/PlayerControllerScripts.cs
@@ -11,19 +11,21 @@ public class PlayerControllerScripts : MonoBehaviour
     public GameObject bullet;
     public Vector2 offset = new Vector2(0.4f, 0.4f);
 
+    bool grounded = false;
+    public Transform groundCheck;
+    public float groundRadius = 0.5f;
+    public LayerMask whatIsGround;
+
     public float maxSpeed = 10f;
     public float jumpForce = 450f;
     public bool facingRight = true;
     public Rigidbody2D Body;
     Animator anim;
     Rigidbody2D myRigidbody;
-    int JumpCount = 0;
-    public int MaxJumps = 1; //Maximum amount of jumps (i.e. 2 for double jumps)
 
     // Use this for initialization
     void Start()
     {
-        JumpCount = MaxJumps;
         anim = GetComponent<Animator>();
         Body = GetComponent<Rigidbody2D>();
         myRigidbody = GetComponent<Rigidbody2D>();
@@ -52,23 +54,21 @@ public class PlayerControllerScripts : MonoBehaviour
             CheckForShot();
 
         }
-
-        if (myRigidbody.velocity.y < 0)
-        {
-            anim.SetBool("HasLanded", false);
-            anim.SetTrigger("Falling");
-        }
-        else
-        {
-            anim.ResetTrigger("Falling");
-        }
     }
 
+    private void CheckForJump()
+    {
+        if (grounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
+        {
+            Jump();
+        }
+    }
 
     // Update is called at the same time as the physics engine
     void FixedUpdate()
     {
-
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
     }
 
     void Flip()
@@ -78,16 +78,9 @@ public class PlayerControllerScripts : MonoBehaviour
         flipX.flipX = !facingRight;
     }
 
-    void CheckForJump()
+    public void Tester()
     {
-        if (JumpCount > 0)
-        {
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                anim.SetBool("HasLanded", false);
-                Jump();
-            }
-        }
+        anim.SetTrigger("TEST");
     }
 
     private void CheckForShot()
@@ -96,22 +89,17 @@ public class PlayerControllerScripts : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.X))
             {
-                anim.ResetTrigger("Falling_Shoot");
                 if (anim.GetCurrentAnimatorStateInfo(0).IsName("Player_Idle"))
                 {
                     Debug.Log("IDLE Shoot");
                     //Instantiate(bullet, (Vector2)transform.position + offset * transform.localScale.x, Quaternion.identity);
                     anim.SetTrigger("Idle_Shoot");
                 }
-                else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Player_Jump"))
+                else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Player_Jump&Fall"))
                 {
-                    Debug.Log("JUMPING Shoot");
                     anim.SetTrigger("Jumping_Shoot");
-                }
-                else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Player_Falling"))
-                {
-                    Debug.Log("FALLING Shoot");
-                    anim.SetTrigger("Falling_Shoot");
+                    anim.Play("Player_Shoot_In_Air");
+                    Debug.Log("JUMPING Shoot");
                 }
                 else if (anim.GetCurrentAnimatorStateInfo(0).IsName("Player_Run"))
                 {
@@ -123,21 +111,18 @@ public class PlayerControllerScripts : MonoBehaviour
     }
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            anim.SetBool("HasLanded", true);
-            anim.ResetTrigger("Falling");
-            anim.ResetTrigger("Jump");
-            JumpCount = MaxJumps;
-        }
-    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Ground")
+    //    {
+    //        JumpCount = MaxJumps;
+    //    }
+    //}
 
     void Jump()
     {
         myRigidbody.AddForce(new Vector2(0, jumpForce));
-        anim.SetTrigger("Jump");
-        JumpCount -= 1;
+        //anim.SetTrigger("Jump");
+        //JumpCount -= 1;
     }
 }
