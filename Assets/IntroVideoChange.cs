fa2c6eb89c5ec633a1eec539a9ac0f4313964c89
wibo <william.boquist@gmail.com>
2017-09-15 09:29:17 +0200

    Near complete

---
+++ Assets/IntroVideoChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroVideoChange : MonoBehaviour
+{
+    public string levelToLoad;
+    private float timer = 6f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+
+    public void skip()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
